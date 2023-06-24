7ff1e91cb43e2a51eb9c187044f96e518f49212c
Goldenprevue <goldenprevue@gmail.com>
2023-06-24 23:34:48 +0900

    upstream pull

--- src/components/create/Container.tsx
+++ src/components/create/Container.tsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import ImageUpLoad from "./ImageUpLoad";
 import Player from "components/common/player/Player";
 import Head from "./Head";
+import Description from "./Description";
 
 export default function Container() {
+  const [nextMove, setNextMove] = useState<number>(2);
+  // switch (nextMove) {
+  //   case 1:
+  // }
   return (
     <div className="w-[390px] bg-slate-50 mx-auto flex-col mt-40 bg">
       <>
         <Head />
-        <Player />
+        {nextMove === 1 ? <Player /> : ""}
+        {nextMove === 2 ? <Description /> : ""}
         <ImageUpLoad />
       </>
     </div>
