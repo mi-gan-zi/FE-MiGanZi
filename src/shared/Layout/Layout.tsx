6162d8a1a52211c152657790f0f5777e0d86c842
pangkyu <baesense@naver.com>
2023-06-25 00:08:09 +0900

    Style : 레이아웃 컴포넌트 width 수정

--- src/shared/Layout/Layout.tsx
+++ src/shared/Layout/Layout.tsx
@@ -1,15 +1,15 @@
-import React from "react"
-import Footer from "./Footer"
-import Header from "./Header"
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
 
 const Layout = (props: { children: React.ReactNode }) => {
   return (
-    <div className="w-[24rem] mx-auto border">
+    <div className="w-[390px] mx-auto border ">
       <Header />
       <main>{props.children}</main>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
