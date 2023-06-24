fd354d175f97d62740e05818cfb4b638dde56a99
Goldenprevue <goldenprevue@gmail.com>
2023-06-25 05:44:31 +0900

    :zap: 생성페이지 주소선택 & 사진업로드

--- src/components/common/player/Player.tsx
+++ src/components/common/player/Player.tsx
@@ -20,7 +20,7 @@ export default function Player(props: PropsType) {
               <div className="bg-[#007DF0] w-[16px] h-[16px] "></div>
             </div>
             <div className="rounded-full bg-[#007DF0] w-[40px] h-[40px] flex items-center justify-center">
-              <div className="border-b-[10px] border-t-[10px] border-r-0 border-l-[16px] ml-1 border-solid border-b-transparent border-t-transparent border-r-transparent border-l-white w-0 h-0"></div>
+              <div className="border-b-[10px] border-t-[10px] border-r-0 border-l-[16px] ml-1 border-solid border-b-st-trans border-t-st-trans border-r-transparent border-l-st-white w-0 h-0"></div>
             </div>
           </div>
         </article>
--- src/components/create/ImageUpLoad.tsx
+++ src/components/create/ImageUpLoad.tsx
@@ -1,9 +1,70 @@
-import React from "react";
+import { UpLoadIcon } from "assets/Icon";
+import React, { useRef, useState } from "react";
 
 export default function ImageUpLoad() {
+  const [img, setImg] = useState<string>("");
+  const ref = useRef<HTMLInputElement>(null);
+  const handleCreateIMG = () => {
+    const file = ref.current?.files?.[0];
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setImg(reader.result as string);
+    };
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <div>
-      <div className="">image</div>
+      <h1 className="text-[20px] font-semibold">업로드할 사진 선택</h1>
+      <div
+        className={
+          "pre-img w-[350px] h-[467px] flex-col justify-center items-center flex " +
+          (img ? "" : "border-dashed border-st-gray-05 border-[1px] rounded-lg")
+        }
+      >
+        {img ? (
+          <img
+            src={img}
+            alt=""
+            className=" max-w-xl rounded-md w-full h-auto aspect-[4/3] object-cover"
+          />
+        ) : (
+          <div className="items-center justify-center flex-col flex gap-1">
+            <div className="opacity-50 flex items-center flex-col">
+              <UpLoadIcon />
+              <p className="text-[#007DF0] text-[18px] font-bold ">
+                사진 업로드
+              </p>
+            </div>
+            <div className=" flex-col flex justify-center items-center">
+              <p>당신만 아는 장소를 업로드 해주세요</p>
+              <p>3:4 비율 사진을 추천해요.</p>
+            </div>
+          </div>
+        )}
+      </div>
+      <label
+        className="block w-[350px] h=[50px] py-2 rounded-lg text-center bg-st-gray-05 text-st-white mt-3"
+        htmlFor="file-upload"
+      >
+        사진 업로드
+      </label>
+      <input
+        type="file"
+        className="hidden"
+        id="file-upload"
+        onChange={handleCreateIMG}
+        ref={ref}
+      />
+      <button
+        onClick={() => {
+          setImg("");
+        }}
+      >
+        취소
+      </button>
     </div>
   );
 }
