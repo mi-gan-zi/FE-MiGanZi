0acb8f56dd5ade524f1dc906277369ad3973acb3
pangkyu <baesense@naver.com>
2023-06-24 22:30:31 +0900

    Style : UI위치 설정

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,5 +2,27 @@ import React from "react";
 import Layout from "shared/Layout/Layout";
 
 export default function Main() {
-  return <Layout>브랜치 테스트</Layout>;
+  return (
+    <>
+      <Layout>
+        <div>(유저)가 뽑은 베스트 아티클</div>
+        <div>----------</div>
+        <div className="flex flex-row">
+          <div>(may 21)</div>
+          <div>|</div>
+          <div>(view data)</div>
+        </div>
+        <div className="flex flex-row">
+          <div>(이미지 들어갈 곳)</div>
+          <div>(유저네임)</div>
+        </div>
+        <div>
+          이미지 들어갈 곳 - 내부에 지도, 태그, 텍스트 내용 최대 2줄.
+          가로방향으로 스크롤
+        </div>
+        <div>새로 작성된 아티클을 확인해보세요</div>
+        <div>이미지 가로 3 세로 무한스크롤</div>
+      </Layout>
+    </>
+  );
 }
