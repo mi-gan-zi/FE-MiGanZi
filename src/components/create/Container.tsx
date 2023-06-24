ac653ddd44070d2d45d15e2400ec19b96450d227
Goldenprevue <goldenprevue@gmail.com>
2023-06-24 22:36:59 +0900

    :art: 플레이어 기본레아아웃 추가 & 프롭스 디폴트 추가

--- src/components/create/Container.tsx
+++ src/components/create/Container.tsx
@@ -6,9 +6,11 @@ import Head from "./Head";
 export default function Container() {
   return (
     <div className="w-[390px] bg-slate-50 mx-auto flex-col mt-40 bg">
-      <Head />
-      <MusicSelect />
-      <ImageUpLoad />
+      <>
+        <Head />
+        <MusicSelect />
+        <ImageUpLoad />
+      </>
     </div>
   );
 }
