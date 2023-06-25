6295228ece956730f1b29fc868417dc39d41b317
Blue-Kite <bluelemon112@gmail.com>
2023-06-25 22:31:10 +0900

    수정

--- src/components/common/player/Player.tsx
+++ src/components/common/player/Player.tsx
@@ -1,60 +1,30 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import moment from 'moment';
+
 interface PropsType {
   song?: string;
   artist?: string;
-  playList: any;
+  playList?: any;
+  playTime?: string;
+  onStartPlay?(): void;
+  onStopPlay?(): void;
 }
 export default function Player(props: PropsType) {
-  const { song = "Happay", artist = "방랑자", playList } = props;
-  const [playing, setPlaying] = useState(false);
-  const [totalTime, setTotalTime] = useState(0);
-  const [current, setCurrentTime] = useState(0);
-  const [audio, setAudio] = useState(new Audio());
-
-  console.log(playList);
-  useEffect(() => {
-    playing ? audio.play() : audio.pause();
-    if (playing) {
-      setTotalTime(audio.duration);
-      audio.addEventListener("timeupdate", () => {
-        setCurrentTime(audio.currentTime);
-      });
-    }
-  }, [playing]);
-  useEffect(() => {
-    if (playList) {
-      setAudio(new Audio(playList));
-    }
-  }, [playList]);
-
-  const onStartPlay = () => {
-    playing || setPlaying(true);
-  };
-
-  const onStopPlay = () => {
-    // playing && audio.pause();
-    playing && setPlaying(false);
-  };
+  const { song = "Happay", artist = "방랑자", playTime = "01: 20" , onStartPlay, onStopPlay } = props;
   return (
     <div className="bg-yellow-300 flex justify-end">
-      <div className="bg-[#F5F4F3] border-st-gray-03 border-[1px] w-[370px] h-[157px] rounded-l-xl py-4 flex justify-between px-6">
+      <div className="bg-[#F5F4F3] border-gray-200 border-[1px] w-[370px] h-[157px] rounded-l-xl py-4 flex justify-between px-6">
         <article className="left_box flex-col flex">
           <div className="description flex-col flex gap-1 mb-2">
             <p>{song}</p>
             <p>{artist}</p>
-            <p className="thin">{current}</p>
+            <p className="thin">{moment(Number(playTime) * 1000).format("mm:ss")}</p>
           </div>
           <div className="button_box flex gap-3">
-            <div
-              className="stop-button border-[#007DF0] border-[1px] w-[40px] h-[40px] rounded-full flex items-center justify-center cursor-pointer"
-              onClick={onStopPlay}
-            >
+            <div className="border-[#007DF0] border-[1px] w-[40px] h-[40px] rounded-full flex items-center justify-center" onClick={onStopPlay}>
               <div className="bg-[#007DF0] w-[16px] h-[16px] "></div>
             </div>
-            <div
-              className="play-button rounded-full bg-[#007DF0] w-[40px] h-[40px] flex items-center justify-center cursor-pointer"
-              onClick={onStartPlay}
-            >
+            <div className="rounded-full bg-[#007DF0] w-[40px] h-[40px] flex items-center justify-center" onClick={onStartPlay}>
               <div className="border-b-[10px] border-t-[10px] border-r-0 border-l-[16px] ml-1 border-solid border-b-st-trans border-t-st-trans border-r-transparent border-l-st-white w-0 h-0"></div>
             </div>
           </div>
