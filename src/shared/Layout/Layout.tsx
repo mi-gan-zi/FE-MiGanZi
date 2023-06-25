9a83dc32ee294e35da1993059d67a58007c0b810
crystal025 <109053875+crystal025@users.noreply.github.com>
2023-06-25 22:40:08 +0900

    :zap: background img

--- src/shared/Layout/Layout.tsx
+++ src/shared/Layout/Layout.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
+import background from "../../assets/background.png";
+import { url } from "inspector";
 
 const Layout = (props: { children: React.ReactNode }) => {
   return (
-    <div className="w-[390px] mx-auto border ">
-      <Header />
-      <main>{props.children}</main>
-      <Footer />
-    </div>
+    <>
+      <div
+        className="w-full h-full bg-center bg-cover bg-no-repeat"
+        style={{ backgroundImage: `url(${background})`, zIndex: -99 }}
+      >
+        <div className="w-[390px] mx-auto border z-10 bg-st-white">
+          <Header />
+          <main>{props.children}</main>
+          <Footer />
+        </div>
+      </div>
+    </>
   );
 };
 
