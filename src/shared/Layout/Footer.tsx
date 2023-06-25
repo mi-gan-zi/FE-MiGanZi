e7bd2f93c90579eb938bd0d55b1cb9b0faef340a
crystal025 <109053875+crystal025@users.noreply.github.com>
2023-06-25 17:03:28 +0900

    :zap: EDIT create Header

--- src/shared/Layout/Footer.tsx
+++ src/shared/Layout/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return <div className="w-full h-[6rem] bg-slate-50"></div>;
+  return <div className="w-full h-[6rem] bg-st-gray-07"></div>;
 };
 
 export default Footer;
