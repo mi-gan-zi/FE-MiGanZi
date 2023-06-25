fff1029425b4c1f181389c60abda32d1fc97b483
crystal025 <109053875+crystal025@users.noreply.github.com>
2023-06-25 20:52:34 +0900

    :zap: EDIT create textarea

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,13 +1,17 @@
 import { NicknameComponent } from "components/signin/NicknameComponent";
 import SignInComponent from "components/signin/SignInComponent";
 import React, { useState } from "react";
+import Footer from "shared/Layout/Footer";
+import Header from "shared/Layout/Header";
 
 export default function SignIn() {
   const [status, setStatus] = useState<Number>(0);
   return (
     <>
+      <Header />
       {status === 0 && <SignInComponent setStatus={setStatus} />}
       {status === 1 && <NicknameComponent />}
+      <Footer />
     </>
   );
 }
--- src/shared/Layout/Footer.tsx
+++ src/shared/Layout/Footer.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { ReactComponent as Main } from "../../assets/footer_main.svg";
+import { ReactComponent as Search } from "../../assets/footer_search.svg";
+import { ReactComponent as Upload } from "../../assets/footer_upload.svg";
+import { ReactComponent as User } from "../../assets/footer_mypage.svg";
 
 const Footer = () => {
-  return <div className="w-full h-[6rem] bg-st-gray-07"></div>;
+  return (
+    <div className="w-full h-[6rem] mt-5 bg-st-gray-02 flex">
+      <div className="w-[98px] flex py-[4px] px-[4px] flex-col justify-center items-center">
+        <Main />
+        <p className="text-st-gray-05 text-sm">메인</p>
+      </div>
+      <div className="w-[98px] flex py-[4px] px-[4px] flex-col justify-center items-center">
+        <Search />
+        <p className="text-st-gray-05 text-sm">검색</p>
+      </div>
+      <div className="w-[98px] flex py-[4px] px-[4px] flex-col justify-center items-center">
+        <Upload />
+        <p className="text-st-gray-05 text-sm">업로드</p>
+      </div>
+      <div className="w-[98px] flex py-[4px] px-[4px] flex-col justify-center items-center">
+        <User />
+        <p className="text-st-gray-05 text-sm">마이페이지</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
