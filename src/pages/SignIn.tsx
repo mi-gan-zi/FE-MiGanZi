382d0fb10b1469ca84caf73536d0e8d5dc19c6f5
new-crystal <oj3339@naver.com>
2023-06-27 12:53:54 +0900

    :zap: map marker address 컴포넌트 추가

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,3 +1,4 @@
+import MapMark from "components/map/MapMark";
 import { NicknameComponent } from "components/signin/NicknameComponent";
 import SignInComponent from "components/signin/SignInComponent";
 import React, { useState } from "react";
@@ -5,10 +6,11 @@ import Footer from "shared/Layout/Footer";
 import Header from "shared/Layout/Header";
 
 export default function SignIn() {
-  const [status, setStatus] = useState<Number>(0);
+  const [status, setStatus] = useState<Number>(3);
   return (
     <>
       {/* <Header /> */}
+      <MapMark/>
       {status === 0 && <SignInComponent setStatus={setStatus} />}
       {status === 1 && <NicknameComponent />}
       {/* <Footer /> */}
