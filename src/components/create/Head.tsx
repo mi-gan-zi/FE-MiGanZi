0395d667f230a9c4fc3b806a7ce0759817faada0
crystal025 <109053875+crystal025@users.noreply.github.com>
2023-06-29 22:31:57 +0900

    :zap: upload 기능 완성

--- src/components/create/Head.tsx
+++ src/components/create/Head.tsx
@@ -1,11 +1,19 @@
 import React, { Dispatch, SetStateAction } from "react";
 import { ReactComponent as Pre } from "../../assets/pre.svg";
 
-type NextProps = {
+export type NextProps = {
   setNextMove: Dispatch<SetStateAction<number>>;
+  image: boolean;
 };
 
-export default function Head({ setNextMove }: NextProps) {
+export default function Head({ setNextMove, image }: NextProps) {
+  const onClickNextButton = () => {
+    if (image) {
+      alert("사진을 업로드 해주세요!");
+    } else {
+      setNextMove((pre) => pre + 1);
+    }
+  };
   return (
     <div className="w-full pb-4 flex items-center justify-between px-5 border-b-[1px] border-st-gray-03">
       <div className="flex">
@@ -16,7 +24,7 @@ export default function Head({ setNextMove }: NextProps) {
         <p className="translate-x-4 font-bold">게시글 작성</p>
       </div>
       <div>
-        <button onClick={() => setNextMove((pre) => pre + 1)}>다음</button>
+        <button onClick={onClickNextButton}>다음</button>
       </div>
     </div>
   );
