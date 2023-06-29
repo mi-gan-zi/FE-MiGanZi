f84df2c8e1e92404516fc081efee12107906239a
crystal025 <109053875+crystal025@users.noreply.github.com>
2023-06-29 22:38:59 +0900

    :zap: token으로 로그인 여부 판별하기

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,11 +1,18 @@
 import MapMark from "components/common/map/MapMark";
 import { NicknameComponent } from "components/signin/NicknameComponent";
 import SignInComponent from "components/signin/SignInComponent";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Footer from "shared/Layout/Footer";
 import Header from "components/signin/Header";
+import { useNavigate } from "react-router-dom";
 
 export default function SignIn() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      navigate("/");
+    }
+  }, []);
   return (
     <>
       <Header />
