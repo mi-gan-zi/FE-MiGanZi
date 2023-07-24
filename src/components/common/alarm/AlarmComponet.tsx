ad80f2c57780c08b7f92f5f9479a0de62e990fc4
crystal025 <109053875+crystal025@users.noreply.github.com>
2023-07-24 22:33:09 +0900

    Fix myposts, mycomment 수정

--- src/components/common/alarm/AlarmComponet.tsx
+++ src/components/common/alarm/AlarmComponet.tsx
@@ -1,23 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export const AlarmComponent = ({ post }: any) => {
-  const clock = post?.createdDate.split(" ")[1];
-  const day = post?.createdDate.split(" ")[0].split(".");
-  const time = day[0] + "년" + day[1] + "월" + day[2] + "일 " + clock;
+type PostProps = {
+  post: {
+    createdDate?: string | undefined;
+    title?: string | undefined;
+    message?: string | undefined;
+    content?: string | undefined;
+    image_url?: string | undefined;
+    modifiedDate?: string | undefined;
+  };
+};
+
+export const AlarmComponent = ({ post }: PostProps) => {
+  const [time, setTime] = useState<string | undefined>("");
+
+  const getTimes = (times: string) => {
+    const clock = times.split(" ")[1];
+    const day = times.split(" ")[0]?.split(".");
+    const newTime =
+      day && day[0] + "년" + day[1] + "월" + day[2] + "일 " + clock;
+    setTime(newTime);
+  };
+
+  useEffect(() => {
+    if (post.createdDate) {
+      getTimes(post.createdDate);
+    } else if (post.modifiedDate) {
+      getTimes(post.modifiedDate);
+    }
+  }, []);
 
   return (
-    <div
-      className="w-[350px] h-[94px] px-5 py-4 flex items-center justify-start"
-      key={post?.id}
-    >
-      <img
-        className="w-[64px] h-[64px] rounded mr-4"
-        src="https://storage.googleapis.com/miganzi-bucket/profile_image.png"
-        alt=""
-      ></img>
+    <div className="w-[350px] h-[94px] px-5 py-4 flex items-center justify-start">
+      {post.image_url !== undefined ? (
+        <>
+          <img
+            className="w-[64px] h-[64px] rounded mr-4"
+            src={`${post.image_url}`}
+            alt=""
+          ></img>
+        </>
+      ) : (
+        <>
+          <img
+            className="w-[64px] h-[64px] rounded mr-4"
+            src="https://storage.googleapis.com/miganzi-bucket/profile_image.png"
+            alt=""
+          ></img>
+        </>
+      )}
+
       <div className="flex flex-col items-start justify-between">
-        <p className="text-st-gray-10 text-base font-medium">{post.title}</p>
-        <p className="text-st-gray-08 text-sm font-normal">{post.message}</p>
+        {post.title ? (
+          <p className="text-st-gray-10 text-base font-medium">{post.title}</p>
+        ) : null}
+        {post.message ? (
+          <p className="text-st-gray-08 text-sm font-normal">{post.message}</p>
+        ) : (
+          <p className="text-st-gray-08 text-sm font-normal">{post.content}</p>
+        )}
         <p className="text-st-gray-05 text-xs font-semibold">{time}</p>
       </div>
     </div>
