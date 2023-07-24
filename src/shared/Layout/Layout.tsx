341d2894897564277f9c73d326e320394ea443e9
Goldenprevue <goldenprevue@gmail.com>
2023-07-24 18:22:02 +0900

    Feat: scroll bar 제거

--- src/shared/Layout/Layout.tsx
+++ src/shared/Layout/Layout.tsx
@@ -5,12 +5,12 @@ import Header from "./Header";
 const Layout = (props: { children: React.ReactNode }) => {
   return (
     <div className="bg-st-white w-[390px]  mx-auto overflow-y-scroll ">
-      <div className="overflow-y-scroll h-screen bg-st-gray-09">
-        <Header />
+      <Header />
+      <div className="overflow-y-scroll  h-screen bg-st-gray-09 scroll">
         <div className="h-[60px]"></div>
-        <main className="">{props.children}</main>
-        <Footer />
+        <main>{props.children}</main>
       </div>
+      <Footer />
     </div>
   );
 };
