079e7f5b3b7d945a5ea534687231b8c832df8753
Goldenprevue <goldenprevue@gmail.com>
2023-07-26 15:16:47 +0900

    Feat: Access expier_time 검증

--- src/repository/LocalTokenRepository.ts
+++ src/repository/LocalTokenRepository.ts
@@ -2,22 +2,36 @@ import constants from "utils/consts/LocalRepo";
 export class LocalTokenRepository {
   private refresh_token = constants.REFRESH_KEY;
   private access_token = constants.ACCESS_TOKEN_KEY;
+  private expier_time = constants.EXPIER_TIME;
   private nickname = constants.NiCK_NAME_KEY;
 
   setRefresh(refresh_token: string) {
     localStorage.setItem(this.refresh_token, refresh_token);
   }
 
-  setToken(token: string) {
-    localStorage.setItem(this.refresh_token, token);
+  setAccess(access_token: string, expier_time: number) {
+    localStorage.setItem(this.access_token, access_token);
+    localStorage.setItem(this.access_token, expier_time.toString());
   }
 
   setNickName(nickname: string) {
     localStorage.setItem(this.nickname, nickname);
   }
 
-  get(): string | null {
-    return localStorage.getItem(this.refresh_token);
+  getAccess(): string | null {
+    const accessToken = localStorage.getItem(this.access_token);
+    const getExpierTimeStr = localStorage.getItem(this.expier_time);
+    if (!getExpierTimeStr || !accessToken) return null;
+
+    const expierToNum = Number(getExpierTimeStr);
+    const currentTime = Date.now();
+
+    // TODO:  reissue api를 정의
+    const newAccess = () => {
+      return "access";
+    };
+    // TODO
+    return expierToNum < currentTime ? newAccess() : accessToken;
   }
   remove() {
     localStorage.removeItem(this.access_token);
--- src/utils/consts/LocalRepo.ts
+++ src/utils/consts/LocalRepo.ts
@@ -2,10 +2,12 @@ const constants = {
   REFRESH_KEY: "",
   ACCESS_TOKEN_KEY: "",
   NiCK_NAME_KEY: "",
+  EXPIER_TIME: "",
 };
 
 constants.REFRESH_KEY = "refresh_token";
 constants.ACCESS_TOKEN_KEY = "access_token";
+constants.EXPIER_TIME = "expier_time";
 constants.NiCK_NAME_KEY = "nickname";
 
 export default constants;
