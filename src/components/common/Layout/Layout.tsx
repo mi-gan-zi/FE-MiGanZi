47e14cacf090ffdd055b0e41893d355223139be5
0gunkim <goldenprevue@gmail.com>
2023-08-04 03:27:28 +0900

    Refactor: Header삭제

--- src/components/common/Layout/Layout.tsx
+++ src/components/common/Layout/Layout.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Footer from "./Footer";
-import Header from "./Header";
+// import Header from "./Header";
 
 const Layout = (props: { children: React.ReactNode }) => {
   return (
     <div className="bg-st-white w-[390px] h-screen  mx-auto">
       <div className="overflow-y-scroll  scroll h-screen pb-28">
-        <Header />
+        {/* <Header /> */}
         <main>{props.children}</main>
       </div>
       <div className="fixed bottom-0 w-[390px]  bg-st-white pt-2">
