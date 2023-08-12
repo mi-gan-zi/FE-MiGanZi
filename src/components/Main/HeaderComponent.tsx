e6a011c0f4565c3026ad108f8a120c0998f923e7
pangkyu <baesense@naver.com>
2023-08-12 14:12:21 +0900

    Feat : 헤더 컴포넌트 라우팅 추가
    버튼 클릭 시 login, user 컴포넌트로 라우팅 추가

--- src/components/Main/HeaderComponent.tsx
+++ src/components/Main/HeaderComponent.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import { ReactComponent as Logo } from "../../assets/miganzi.svg";
 import { ReactComponent as Notification } from "../../assets/notification.svg";
 import { ReactComponent as MyProfile } from "../../assets/myProfile.svg";
-
+import { useNavigate } from "react-router-dom";
 const HeaderComponent = () => {
   const isLoggedIn = !!localStorage.getItem("access_token");
+  const navigate = useNavigate();
+
+  const handleOnClick = (props: string) => {
+    navigate(`/${props}`);
+  };
+
   return (
     <div className="flex w-[100%] pb-4 flex-col items-center justify-center px-5 border-b-[1px] border-st-gray-03 mt-4">
       <div className="flex w-[90%] justify-between items-center ml-[25px]">
@@ -12,7 +18,10 @@ const HeaderComponent = () => {
         <div className="flex flex-row items-center">
           {isLoggedIn ? (
             <>
-              <div className="w-[34px] cursor-pointer ">
+              <div
+                className="w-[34px] cursor-pointer"
+                onClick={() => handleOnClick("user")}
+              >
                 <MyProfile />
               </div>
               <div className="w-[34px] cursor-pointer ml-[5px]">
@@ -20,7 +29,12 @@ const HeaderComponent = () => {
               </div>
             </>
           ) : (
-            <p>로그인</p>
+            <p
+              className="cursor-pointer"
+              onClick={() => handleOnClick("login")}
+            >
+              로그인
+            </p>
           )}
         </div>
       </div>
