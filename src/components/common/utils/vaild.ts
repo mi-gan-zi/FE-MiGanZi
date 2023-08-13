aed31e174980c1a126e21f327e3ac7d8d79d9966
crystal025 <109053875+crystal025@users.noreply.github.com>
2023-08-13 12:44:15 +0900

    Refactor:비밀번호 정규식 수정

--- src/components/common/utils/vaild.ts
+++ src/components/common/utils/vaild.ts
@@ -5,6 +5,6 @@ export const vaildNick = (nickName: VaildType["nickName"]) => {
   return regex.test(nickName);
 };
 export const validPassword = (password: VaildType["password"]) => {
-  const regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+  const regex = /^(?=.[A-Za-z])(?=.\d)[A-Za-z\d]{8,16}$/;
   return regex.test(password);
 };
